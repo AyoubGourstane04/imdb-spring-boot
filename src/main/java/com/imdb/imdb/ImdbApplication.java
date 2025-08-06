c9284c545301d85b01a0516ab34726c79bff58b7
AyoubGourstane04 <ayoubgourstan@gmail.com>
2025-08-06 02:17:34 +0100

    latest commit

--- src/main/java/com/imdb/imdb/ImdbApplication.java
+++ src/main/java/com/imdb/imdb/ImdbApplication.java
@@ -2,10 +2,8 @@ package com.imdb.imdb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 
 @SpringBootApplication
--- src/main/java/com/imdb/imdb/services/FilmService.java
+++ src/main/java/com/imdb/imdb/services/FilmService.java
@@ -10,7 +10,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
-import org.bson.json.JsonObject;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
